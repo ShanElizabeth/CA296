f88282d7f68e9c54ec563c2b5e19d5ee6220a92e
ShanElizabeth <42028485+ShanElizabeth@users.noreply.github.com>
2018-08-13 13:20:59 +0100

    Add files via upload

---
+++ FillArrayRandomNumbers/main.asm
@@ -0,0 +1,42 @@
+;
+; CA296 Console Template
+; Version 1
+;
+
+;
+; Preamble
+;
+
+.586
+.model flat,stdcall
+.stack 4096
+option casemap:none
+
+include     p:\masm32\include\windows.inc
+include     p:\masm32\include\kernel32.inc
+include		p:\masm32\include\user32.inc
+include		p:\masm32\include\msvcrt.inc
+include		p:\masm32\include\ca296.inc
+
+includelib p:\masm32\lib\ca296.lib
+includelib kernel32.lib
+includelib user32.lib
+includelib msvcrt.lib
+
+
+;
+; Data & Code
+;
+
+.data
+len=10
+a DWORD len DUP(0)
+
+.code 
+	main:nop
+	invoke fillWithRandomValues,addr a,len,100
+	invoke writeArray,addr a,len
+	invoke Sleep,500
+
+	invoke ExitProcess,0
+end main
